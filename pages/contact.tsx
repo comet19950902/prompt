a8ac8d4e5abb353ac4ea0b749e3865b1a99bae31
NEVO-SAY-NEVO <dev0825conscientious@gmail.com>
2023-08-26 18:04:14 +0900

    fix

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
-
-export default function contact() {
+import Header from '@/components/global/header'
+import Footer from '@/components/global/footer'
+import ContactUs from '@/components/contact/Contact'
+export default function price() {
   return (
-    <div></div>
+    <div className='bg-[white]'>
+        <Header />
+        <ContactUs />
+        <Footer />
+    </div>
   )
 }
