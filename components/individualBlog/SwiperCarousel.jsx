5cc5951068728f6643fce660d6a00ca4af2e2385
NEVO-SAY-NEVO <dev0825conscientious@gmail.com>
2023-08-29 20:13:05 +0900

    fix

--- components/individualBlog/SwiperCarousel.jsx
+++ components/individualBlog/SwiperCarousel.jsx
@@ -44,6 +44,36 @@ const projects = [
         description:
             "Astronaud helps YouTube Networks, YouTube Channels, Artists, Musicians and many others. I was in charge of UI/UX design and development.",
         image: "/projects/8.png",
+    },
+    {
+        name: "Crypto Legions",
+        description:
+            "Crypto Legions is a cutting edge Play-To-Earn NFT game on the BSC network. Bloodstone is the native utility token that's used for: Minting in-game NFTs. I was on charge of frontend development and smart contract",
+        image: "/projects/1.jpg",
+    },
+    {
+        name: "GODz of Solaria",
+        description:
+            "The world's first DAO operated NFT Incubator, launchpad and game platform. I was on charge of frontend development of mint and sacrifice pages, also smart contract and backend development for sacrifice",
+        image: "/projects/2.png",
+    },
+    {
+        name: "Neon Sabers",
+        description:
+            "Neon Sabers is one stop shop for highly premium and durable Neon Lightsabers.Our lightsabers are highly customizable, you can choose the color of Hilt, type of blade and attach accessories of your choice.",
+        image: "/projects/6.jpg",
+    },
+    {
+        name: "MailMerge365",
+        description:
+            "A Microsoft 365 plugin and SaaS to enable the sending of mass personalized email using your existing Microsoft tenant mailbox. Frontend developer role.",
+        image: "/projects/4.png",
+    },
+    {
+        name: "Unjabbed Dating",
+        description:
+            "Unjabbed is a unique dating app exclusively for those who have chosen not to receive the COVID-19 vaccine. I was the only developer for this project.",
+        image: "/projects/5.png",
     }
 ];
 
@@ -58,7 +88,7 @@ const SwiperCarousel = () => {
                 loop={true}
                 initialSlide={1}
                 spaceBetween={0}
-                slidesPerView={3}
+                slidesPerView={5}
                 autoplay={{
                     delay: 5000,
                 }}
