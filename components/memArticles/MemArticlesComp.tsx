a89d61d549a57aa6fbc8666cadfaf498682070eb
NEVO-SAY-NEVO <dev0825conscientious@gmail.com>
2023-09-02 03:52:42 +0900

    fix

--- components/memArticles/MemArticlesComp.tsx
+++ components/memArticles/MemArticlesComp.tsx
@@ -12,8 +12,8 @@ const MemArticlesComp:React.FC<Props> = ({title, image}) => {
         </div>
         <div className='grid lg:grid-cols-5 md:grid-cols-3 grid-cols-2 gap-3 py-6 px-4 rounded-2xl shadow-card-upload-black'>
             {image.map((img, index) => (
-                <div className='w-full outline-2 p-1 rounded-lg shadow-card-upload relative'>
-                    <img src={img} key={index} className='w-full h-full rounded-lg' alt='ddd'/>
+                <div className='w-full outline-2 p-1 rounded-lg shadow-card-upload relative' key={index}>
+                    <img src={img} className='w-full h-full rounded-lg' alt='ddd'/>
                     <div className='absolute bottom-0 left-0 w-full bg-[#ffffffcc] justify-between truncate text-[black] rounded-b-lg px-3 py-2 font-semibold flex'>
                         {title}
                     </div>
