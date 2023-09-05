8dbcfbff620af652447431b9d37040d2ccfa9ea8
NEVO-SAY-NEVO <dev0825conscientious@gmail.com>
2023-09-06 01:46:23 +0900

    fix

--- components/freeGPT/FreeGpt.tsx
+++ components/freeGPT/FreeGpt.tsx
@@ -16,7 +16,6 @@
 
 import React, { useState, useEffect } from 'react';
 import ReactMarkdown from "react-markdown";
-import styles from './FreeGpt.module.css'
 
 const MyComponent: React.FC = () => {
   const [markdownContent, setMarkdownContent] = useState('');
--- pages/api/markdown.js
+++ pages/api/markdown.js
@@ -4,5 +4,5 @@ import fs from 'fs'; // Node.js file system module
 
 export default function handler(req, res) {
   const markdownContent = fs.readFileSync('public/home.md', 'utf-8');
-  res.status(200).json({ content: markdownContent });
+  res.status(200).json({ content: markdownContent })
 }
