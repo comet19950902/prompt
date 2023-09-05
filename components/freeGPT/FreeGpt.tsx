e8dc4a7a9e85dd8970ada566c7a26b5a963469d7
NEVO-SAY-NEVO <dev0825conscientious@gmail.com>
2023-09-06 01:17:21 +0900

    fix

--- components/freeGPT/FreeGpt.tsx
+++ components/freeGPT/FreeGpt.tsx
@@ -1,15 +1,38 @@
-import React, { useState, useEffect } from "react";
-// import ReactMarkdown from "react-markdown";
-// import rehypeRaw from "rehype-raw";
-// import gfmMD from "remark-gfm";
+// import React, { useState, useEffect } from "react";
+// // import ReactMarkdown from "react-markdown";
+// // import rehypeRaw from "rehype-raw";
+// // import gfmMD from "remark-gfm";
 // import markdown from "../../docs/home.md";
 
-const FreeGpt = () => {
-    return (
-      <div>
-        {/* <pre>{markdown}</pre> */}
-        <small><i>Import and render markdown using Next.js</i></small>
-      </div>
-    );
-  };
-  export default FreeGpt;
+// const FreeGpt = () => {
+//     return (
+//       <div>
+//         <pre>{markdown}</pre>
+//         <small><i>Import and render markdown using Next.js</i></small>
+//       </div>
+//     );
+//   };
+//   export default FreeGpt;
+
+import React, { useState, useEffect } from 'react';
+import ReactMarkdown from "react-markdown";
+import styles from './FreeGpt.module.css'
+
+const MyComponent: React.FC = () => {
+  const [markdownContent, setMarkdownContent] = useState('');
+
+  useEffect(() => {
+    fetch('/api/markdown')
+      .then(response => response.json())
+      .then(data => setMarkdownContent(data.content));
+  }, []);
+
+  return (
+    <div className={`lg:pt-[90px] px-4 md:px-16 lg:px-40 py-20 shadow-card-upload-black relative flex flex-col min-w-full prose lg:prose-xl text-black before:absolute before:content-[' '] before:top-0 before:left-0 before:bottom-0 before:right-0 before:bg-[url(/gradient/10.jpg)] before:opacity-80 before:bg-cover before:-z-0 fox font-header`}>
+      {markdownContent && <ReactMarkdown className='z-10'>{markdownContent}</ReactMarkdown>}
+    </div>
+  );
+};
+
+export default MyComponent;
+
